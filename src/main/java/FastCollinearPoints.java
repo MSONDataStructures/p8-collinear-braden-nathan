5ad0155d790190b358e1497707023e97d1768413
NathanBreit <ngbreit@icloud.com>
2025-02-23 16:48:21 -0600

    Partially through the FastCollinearPoints Class. Still Stuck on many things.

--- src/main/java/FastCollinearPoints.java
+++ src/main/java/FastCollinearPoints.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 /*************************************************************************
  *   Given a point p, the following method determines
  *   whether p participates in a set of 4 or more collinear points.
@@ -10,20 +11,56 @@
  *************************************************************************/
 public class FastCollinearPoints {
 
+    private Point[] segments;
+    private int numberOfSegmentsHolder = 0;
     public FastCollinearPoints(Point[] points) {
         // finds all line segments containing 4 points
         // TODO: YOUR CODE HERE
+        for (int i = 0; i < points.length; i++) {
+            Point mainPoint = points[i];
+            Point[] toBeSorted = duplicateArray(points);
+            Arrays.sort(toBeSorted, mainPoint.slopeOrder());
+
+            double slopeCheck = mainPoint.slopeTo(toBeSorted[0]);
+            int count = 0;
+            for (int j = 1; j < toBeSorted.length; j++) {
+                double slope = mainPoint.slopeTo(toBeSorted[j]);
+                if (slope == slopeCheck) {
+                    count++;
+                }
+                if (slope != slopeCheck) {
+                    if (count >= 2) {
+                        numberOfSegmentsHolder++;
+                        count = 0;
+                    }
+                }
+                //deals with edge case of a collinear segment being on the end of the for loop
+                if (count >= 2) {
+                    numberOfSegmentsHolder++;
+                    count = 0;
+                }
+                slopeCheck = slope;
+            }
+        }
     }
 
     public int numberOfSegments() {
         // the number of line segments
         // TODO: YOUR CODE HERE
-        return 0;
+        return numberOfSegmentsHolder;
     }
 
     public LineSegment[] segments() {
         // the line segments
         // TODO: YOUR CODE HERE
         return null;
+    }
+
+    public Point[] duplicateArray(Point[] arr) {
+        Point[] newArr = new Point[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
     }
 }
